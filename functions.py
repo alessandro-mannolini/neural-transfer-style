7728b9d49598765bc256a2b590a786a298555e45
Alessandro Mannolini <alessandro.mannolini@crossnection.it>
2023-10-05 21:53:29 +0200

    Completamento del progetto

--- functions.py
+++ functions.py
@@ -1,28 +1,6 @@
 
 import torch
-import torch.nn as nn
-import torchvision.models as models
-
-# modello semplice usato in precedenza
-class VGG(nn.Module):
-    def __init__(self):
-        super(VGG, self).__init__()
-        
-        self.chosen_features = ["0", "5", "10", "19", "28"]
-        self.model = models.vgg19(weights = models.VGG19_Weights.DEFAULT).features[:29]
-        
-    def forward(self, x):
-        features = []
-        
-        for layer_num, layer in enumerate(self.model):
-            x = layer(x)
-            
-            if str(layer_num) in self.chosen_features:
-                features.append(x)
-                
-        return features
-    
-
+# pip list --format=freeze > requirements.txt
 
 # Funzione per normalizzare un tensore
 def normalize(tensor):
